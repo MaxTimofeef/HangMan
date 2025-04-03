c92d91634df71e1e96f3b072544763c1129454ee
maxim <ebanancik9@gmail.com>
2025-04-03 19:05:40 +0500

    feat: add check for letters

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import os
 clear = lambda: os.system("cls")
 
 input("Привет! отгадай слово по буквам\nнажмите ENTER для продолжения")
-
 clear()
+print("Поехали!")
+
+letters = []
 
 word = "автомобиль"
 
-for symd in word:
-    print(symd, end=" ")
+while True:
+    letter = input("Введите букву: ")
+    letters.append(letter)
+    print(letter)
+    for symb in word:
+        if symb in letters:
+            print(symb, end=" ")
+        else:
+            print("*", end=" ")
+    print()
